f10438cd3523dc75919cc6551ed1aaae3b5e49f9
AmandaCacholi <amanda.cacholi@outlook.com>
2021-06-28 17:46:43 -0300

    ADDING CREATION AN EDITING CAPABILITIES to USING A CUSTOM HOME PAGE

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import * as React from "react";
 import jsonServerProvider from 'ra-data-json-server';
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} />
-        <Resource name="users" list={UserList} />
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
--- src/posts.js
+++ src/posts.js
@@ -1,8 +1,30 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+    Filter
+} from 'react-admin';
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
 
 export const PostList = props => (
-    <List {...props}>
+    <List filters={<PostFilter />} {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <ReferenceField source="userId" reference="users">
@@ -13,3 +35,32 @@ export const PostList = props => (
         </Datagrid>
     </List>
 );
+
+const PostTitle = ({ record }) => {
+        return <span>Post {record ? `"${record.title}"` : ''}</span>;
+    };
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+        <Create {...props}>
+            <SimpleForm>
+                <ReferenceInput source="userId" reference="users">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <TextInput source="title" />
+                <TextInput multiline source="body" />
+            </SimpleForm>
+        </Create>
+    );

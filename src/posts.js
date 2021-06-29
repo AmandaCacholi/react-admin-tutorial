d625ac64e4b1b26408c5651a50230e5336c84d02
AmandaCacholi <amanda.cacholi@outlook.com>
2021-06-29 11:47:57 -0300

    finishing tutorial

--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { useMediaQuery } from '@material-ui/core';
 import {
     List,
+    SimpleList,
     Datagrid,
     TextField,
     ReferenceField,
@@ -23,18 +25,30 @@ const PostFilter = (props) => (
     </Filter>
 );
 
-export const PostList = props => (
-    <List filters={<PostFilter />} {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
-        </Datagrid>
-    </List>
-);
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    //secondaryText={record => `${record.views} views`}
+                    //tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField label="User" source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <TextField source="body" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
 
 const PostTitle = ({ record }) => {
         return <span>Post {record ? `"${record.title}"` : ''}</span>;
